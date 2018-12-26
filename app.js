01080bd45d923d21d871e206c53d7574e04a1aa4
Michael Torres <michaelj.torres@yahoo.com>
2018-12-25 20:03:23 -0500

    fizzBuzz Answer 1

--- app.js
+++ app.js
@@ -216,3 +216,31 @@ for (let char of str){  // how to remember this of starts with o
  return maxChar;
 }
 */
+//-----------------------------------------------------------------------
+
+//                                  Fizzbuzz
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+//                                Answer 1 
+function fizzBuzz(n) {
+  for(let i = 1, i <= n ; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      console.log('fizzbuzz');
+    } else if(i % 3 === 0){
+      console.log('fizz');
+    } else if(i % 5 === 0){
+      console.log('buzz')
+    } else {
+      console.log(i);
+    }
