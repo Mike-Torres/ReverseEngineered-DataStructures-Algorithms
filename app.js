f6cfdd2790972278540184d12b81f11c671df584
Michael Torres <michaelj.torres@yahoo.com>
2018-12-25 21:21:39 -0500

    array chunk question answer 1

--- app.js
+++ app.js
@@ -259,3 +259,33 @@ function fizzBuzz(n) {
       console.log(i);
     }
 */
+
+//----------------------------------------------------------------------------
+//                                   Array Chunk Question
+
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+
+//                                          Answer 1
+function chunk(array, size) {
+  let chunked = [];
+  for(let element of array){
+    const last = chunked[chunked.length - 1];
+
+    if(!last || last.length === size){
+      chunk.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
